809d947a792b0db4228ffca4729018a483fb28ca
hrainen <hrainen@hotmail.com>
2017-11-24 03:33:51 -0700

    Much better movement system
    Much more efficient and easy to understand movement system.
    
    Now uses array of length 4 (directions). And adds velocity based on which directions are set to 1;

--- Game shooter/person.js
+++ Game shooter/person.js
@@ -1,9 +1,9 @@
 function Person() {
   this.x = width/2;
   this.y = height/2;
-  this.velocity = 0;
-  this.facing = "N";
+  this.velocity = 10;
   this.isFiring = false;
+  this.dir = [0, 0, 0, 0]; //[N, E, S, W]
 
   this.show = function() {
     fill(255);
@@ -12,26 +12,32 @@ function Person() {
   }
 
   this.move = function() {
-    if (this.facing == "N" && this.velocity != 0){
-      this.y += -this.velocity;
-    }else if (this.facing == "E" && this.velocity != 0){
-      this.x += this.velocity;
-  	} else if (this.facing == "S" && this.velocity != 0){
-      this.y += this.velocity;
-  	} else if (this.facing == "W" && this.velocity != 0){
-      this.x += -this.velocity;
-    } else if (this.facing == "NE" && this.velocity != 0){
-      this.x += this.velocity;
-      this.y += -this.velocity;
-    }else if (this.facing == "NW" && this.velocity != 0){
-      this.x += -this.velocity;
-      this.y += -this.velocity;
-    }else if (this.facing == "SE" && this.velocity != 0){
-      this.x += this.velocity;
-      this.y += this.velocity;
-    }else if (this.facing == "SW" && this.velocity != 0){
-      this.x += -this.velocity;
-      this.y += this.velocity;
+    let xNew = 0;
+    let yNew = 0;
+
+    for (var i = 0; i<this.dir.length; i++){
+      if (i == 0){ // check north
+        if (this.dir[i]){
+          yNew += -this.velocity;
+        }
+      }
+      if (i == 2){ // check south
+        if (this.dir[i]){
+          yNew += this.velocity;
+        }
+      }
+      if(i == 1){ // check east
+        if (this.dir[i]){
+          xNew += this.velocity;
+        }
+      }
+      if(i == 3){ // check west
+        if (this.dir[i]){
+          xNew += -this.velocity;
+        }
+      }
     }
+    this.x += xNew;
+    this.y += yNew;
   }
 }
--- Game shooter/sketch.js
+++ Game shooter/sketch.js
@@ -36,43 +36,16 @@ function draw() {
 }
 
 function keyReleased(){
-	//if player direction = north, and up arrow keyReleased
-	if (player.facing == "N" && keyCode === UP_ARROW){
-		player.velocity = 0;
-	}
-	//if player direction = east, and right arrow keyReleased
-	else if (player.facing == "E" && keyCode === RIGHT_ARROW){
-		player.velocity = 0;
-	}
-	//if player direction = south, and down arrow keyReleased
-	else if (player.facing == "S" && keyCode === DOWN_ARROW){
-		player.velocity = 0;
-	}
-	//if player direction = west, and left arrow keyReleased
-	else if (player.facing == "W" && keyCode === LEFT_ARROW){
-		player.velocity = 0;
-	}
+	if (keyCode === UP_ARROW){
+		player.dir[0] = 0;
+	} else if (keyCode === DOWN_ARROW){
+		player.dir[2] = 0;
+	} else if (keyCode === RIGHT_ARROW){
+		player.dir[1] = 0;
 
-	else if (player.facing == "NE" && keyCode === RIGHT_ARROW){
-		player.facing = "N"
-	}else if (player.facing == "NE" && keyCode === UP_ARROW){
-		player.facing = "E"
-	}else if (player.facing == "NW" && keyCode === LEFT_ARROW){
-		player.facing = "N"
-	}else if (player.facing == "NW" && keyCode === UP_ARROW){
-		player.facing = "W"
-	}	else if (player.facing == "SE" && keyCode === RIGHT_ARROW){
-		player.facing = "S"
-	}else if (player.facing == "SE" && keyCode === DOWN_ARROW){
-		player.facing = "E"
-	}else if (player.facing == "SW" && keyCode === LEFT_ARROW){
-		player.facing = "S"
-	}else if (player.facing == "SW" && keyCode === DOWN_ARROW){
-		player.facing = "W"
+	} else if (keyCode === LEFT_ARROW){
+		player.dir[3] = 0;
 	}
-
-
-
 
 	if (keyCode === 32){ // space = fire
 		player.isFiring = false;
@@ -87,69 +60,20 @@ function keyPressed(){
 		player.isFiring = true;
 	}
 
-	if (keyCode === UP_ARROW  && player.velocity == 0){
-		player.facing = "N";
-		player.velocity = pSpeed;
-	} else if (keyCode === DOWN_ARROW && player.velocity == 0){
-		player.facing = "S";
-		player.velocity = pSpeed;
-	} else if (keyCode === RIGHT_ARROW && player.velocity == 0){
-		//set player direction to right
-		player.facing = "E";
-		player.velocity = pSpeed;
-	} else if (keyCode === LEFT_ARROW && player.velocity == 0){
-		player.facing = "W";
-		player.velocity = pSpeed;
-	}
+	if (keyCode === UP_ARROW){
+		player.isMoving = true;
+		player.dir[0] = 1;
+	} else if (keyCode === DOWN_ARROW){
+		player.isMoving = true;
+		player.dir[2] = 1;
 
-	// if direction = north, velocity != 0, and east keyPressed
-	else if (player.facing == "N" && player.velocity != 0 && keyCode === RIGHT_ARROW){
-		player.facing = "NE";
-		player.velocity = pSpeed;
-	}
-	// if direction = north, velocity != 0, and west keyPressed
-	else if (player.facing == "N" && player.velocity != 0 && keyCode === LEFT_ARROW){
-		player.facing = "NW";
-		player.velocity = pSpeed;
-	}
-	// if direction = north, velocity != 0, and west keyPressed
-	else if (player.facing == "N" && player.velocity != 0 && keyCode === LEFT_ARROW){
-		player.facing = "NW";
-		player.velocity = pSpeed;
-	}
+	} else if (keyCode === RIGHT_ARROW){
+		player.isMoving = true;
+		player.dir[1] = 1;
 
-
-	// if direction = east, velocity != 0, and north keyPressed
-	else if (player.facing == "E" && player.velocity != 0 && keyCode === UP_ARROW){
-		player.facing = "NE";
-		player.velocity = pSpeed;
-	}
-	// if direction = east, velocity != 0, and south keyPressed
-	else if (player.facing == "E" && player.velocity != 0 && keyCode === DOWN_ARROW){
-		player.facing = "SE";
-		player.velocity = pSpeed;
-	}
-
-	// if direction = south, velocity != 0, and east keyPressed
-	else if (player.facing == "S" && player.velocity != 0 && keyCode === RIGHT_ARROW){
-		player.facing = "SE";
-		player.velocity = pSpeed;
-	}
-	// if direction = south, velocity != 0, and west keyPressed
-	else if (player.facing == "S" && player.velocity != 0 && keyCode === LEFT_ARROW){
-		player.facing = "SW";
-		player.velocity = pSpeed;
-	}
-
-	// if direction = west, velocity != 0, and north keyPressed
-	else if (player.facing == "W" && player.velocity != 0 && keyCode === UP_ARROW){
-		player.facing = "NW";
-		player.velocity = pSpeed;
-	}
-	// if direction = west, velocity != 0, and south keyPressed
-	else if (player.facing == "W" && player.velocity != 0 && keyCode === DOWN_ARROW){
-		player.facing = "SW";
-		player.velocity = pSpeed;
+	} else if (keyCode === LEFT_ARROW){
+		player.isMoving = true;
+		player.dir[3] = 1;
 	}
 
 }
